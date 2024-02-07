a58605036a03ec5f200eca3e0db843fefeaa56d9
c.ferrara <chiara.ferrara@stevejobs.academy>
2024-02-07 13:55:23 +0100

    Context example with counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,39 @@
-import React, { useState } from "react";
-import "./App.css";
+import React, { createContext, useContext, useState } from "react";
 
-function Button({onClick} : any) {
- 
+const MyContext  = createContext<any>({});
 
-  //un componente riceve i parametri tramite un oggetto, quando noi definiamo una funzione, passiamo un oggetto che può chiamarsi come vogliamo (props).
-  //Se voglio far sì che cliccando mi spunti un valore randomico devo gestire lo stato nel livello più alto. se fosse un componente più strutturato 
-
-  return (
+function Button(){
+  const {onIncrement} : any = useContext(MyContext);
+  return( 
     <>
-      {" "}
-      <button
-        onClick={onClick} //così passiamo il parametro al bottone
-      >
-        Genera numero
-      </button>
-    </>
-  );
-}
-
-interface PropsLabel {
-  children: JSX.Element; //oppure ReactNode, se mettessumo return ( {null} ) possiamo usare ReactNode
-}
-//se label passa la prop a un subLabel
-function Label({children} : PropsLabel) {
-  return (
-  <>
-  <SubLabel>{children}</SubLabel>
-  </>);
-}
-
-function SubLabel({children}: PropsLabel) {
-console.log('rendering SubLabel')
-  return(
-    <>
-    {children}
+    <button onClick={onIncrement}>increment</button>
     </>
   )
-  
-
 }
 
-function App() {
-  const [number, setNumber] = useState<number>(Math.random());
-  const onClick = () => {
-    const random: number = Math.random();
-    setNumber(random);
-  };
+function Label(){
+
+  const {count} : any = useContext(MyContext);
+  return( 
+    <>
+   <p>{count}</p>
+    </>
+  )
+}
+
+
+function Counter() {
+const [count, setCount] = useState(0);
+
+
 
   return (
-    <>
-      <Button onClick={onClick} />
-      <Label><div>{number}</div></Label>
-    </>
+    <MyContext.Provider value={{count,
+    onIncrement: () => setCount(count +1)}}>
+    <Button/>
+    <Label/>
+    </MyContext.Provider>
   );
 }
 
-export default App;
+export default Counter;
